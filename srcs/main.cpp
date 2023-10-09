e28dfa7040b89df1c477a7e4a0543e6c95b03454
Gillian Pasquette <gpasquet@1E2.42angouleme.fr>
2023-10-09 16:59:33 +0200

    factorisation du main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -9,32 +9,27 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>	
 #include <iostream>
+#include <cstdio>
 
-void *get_in_addr(struct sockaddr *sa)
-{
-	if (sa->sa_family == AF_INET) {
-		return &(((struct sockaddr_in*)sa)->sin_addr);
-	}
-
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
-
-int main(int argc, char **argv) {
+struct addrinfo initHints(void) {
 	struct addrinfo	 hints;
-	struct addrinfo	 *servinfo;
-	struct addrinfo *p;
-	int rv;
-	int listener;
-	// int newfd;
 
-	(void)argc;
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family	= AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
+	return (hints);	
+}
 
-	rv = getaddrinfo(NULL, argv[1], &hints, &servinfo);
+int	initServer(const char *portNb) {
+	int	listener;
+	int rv;
+	struct addrinfo	 hints;
+	struct addrinfo	 *servinfo;
+	struct addrinfo *p;
 
+	hints = initHints();
+	rv = getaddrinfo(NULL, portNb, &hints, &servinfo); 
 	for (p = servinfo; p != NULL; p = p->ai_next) {
 		if ((listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
 			perror("socket:");
@@ -47,80 +42,105 @@ int main(int argc, char **argv) {
 		}
 		break;
 	}
-
 	freeaddrinfo(servinfo);
-
 	if (p == NULL) {
 		std::cout << ("bind error");
 		exit(1);
 	}
 
 	if (listen(listener, 10) == -1) {
-		perror("listen:");
+		std::perror("listen:");
 		exit(1);
 	}
+	return(listener);
+}
 
-	fd_set	master;
-	fd_set	readfds;
-
+void	initFdsets(fd_set &master, fd_set &readFds, int &listener) {
 	FD_ZERO(&master);
-	FD_ZERO(&readfds);
+	FD_ZERO(&readFds);
 	FD_SET(listener, &master);
+}
 
-	int fd_max = listener;
-	int new_fd;
-	int nbytes;
-	socklen_t	addrlen;
-	struct sockaddr_storage remoteaddr;
-	char remoteIP[INET6_ADDRSTRLEN];
+void	newConnection(fd_set &master, int &listener, int &fdMax) {
+	struct sockaddr_storage remoteAddr;
+	socklen_t	addrLen;
+	int newFd;
+	
+	addrLen = sizeof(remoteAddr);
+	newFd = accept(listener, (struct sockaddr *)&remoteAddr, &addrLen);
+	if (newFd == -1) 
+		perror("accept:");
+	else {
+		std::cout << "newFd: " << newFd << std::endl;
+		FD_SET(newFd, &master);
+		if (newFd > fdMax)
+			fdMax = newFd;
+	}
+}
+
+void	receiveError(const int &nbytes, int &socketFd, fd_set &master) {
+	if (nbytes == 0)
+		printf("selectserver: socket %d hung up\n", socketFd);
+	else 
+		perror("recv:");
+	close(socketFd);
+	FD_CLR(socketFd, &master);
+}
+
+void	receiveData(fd_set &master, int &listener, int &fdMax, int &socketFd) {
+	int 	nbytes;
 	char	buf[256];
 
-	while(1) {
-		readfds = master;
-		if (select(fd_max + 1, &readfds, NULL, NULL, NULL) == -1) {
-			perror("Select:");
-			exit(1);
-		}
 
-		for (int i = 0; i <= fd_max; i++) {
-			if (FD_ISSET(i, &readfds)) {
-				if (i == listener) {
-					addrlen = sizeof(remoteaddr);
-					new_fd = accept(listener, (struct sockaddr *)&remoteaddr, &addrlen);
-					if (new_fd == -1) 
-						perror("accept:");
-					else {
-						FD_SET(new_fd, &master);
-						if (new_fd > fd_max)
-							fd_max = new_fd;
-					}
-					printf("selectserver: new connection from %s on socket %d\n", inet_ntop(remoteaddr.ss_family,  get_in_addr((struct sockaddr*)&remoteaddr), remoteIP, INET6_ADDRSTRLEN), new_fd);
-				}
-
-				else {
-					if ((nbytes = recv(i, buf, sizeof(buf), 0)) <= 0) {
-						if (nbytes == 0)
-							printf("selectserver: socket %d hung up\n", i);
-						else 
-							perror("recv:");
-						close(i);
-						FD_CLR(i, &master);
-					}
-					else {
-						for (int j = 0; j <= fd_max; j++) {
-							if (FD_ISSET(j, &master)) {
-								if (j != listener && j != i) {
-									printf("j = %i\n", j);
-									if (send(j, buf, nbytes, 0) == -1)
-										perror("send");
-								}
-							}
-						}
+	if (socketFd == listener) {
+		newConnection(master, listener, fdMax);
+	}
+	else {
+		if ((nbytes = recv(socketFd, buf, sizeof(buf), 0)) <= 0)
+			receiveError(nbytes, socketFd, master);
+		else {
+			for (int j = 0; j <= fdMax; j++) {
+				if (FD_ISSET(j, &master)) {
+					if (j != listener && j != socketFd) {
+						if (send(j, buf, nbytes, 0) == -1)
+							perror("send");
 					}
 				}
 			}
 		}
 	}
+}
+
+void	readLoop(fd_set &master, fd_set &readFds, int &listener, int &fdMax) {
+	for (int i = 0; i <= fdMax; i++) {
+		if (FD_ISSET(i, &readFds))
+			receiveData(master, listener, fdMax, i);
+	}
+}
+
+int main(int argc, char **argv) {
+	int 	listener;
+	fd_set	master;
+	fd_set	readFds;
+	int 	fdMax;
+
+	if (argc != 2) {
+		std::cout << "Port number expected" << std::endl;
+		exit(1);
+	}
+	listener = initServer(argv[1]);
+	initFdsets(master, readFds, listener);
+	fdMax = listener;
+	while(1) {
+		readFds = master;
+		if (select(fdMax + 1, &readFds, NULL, NULL, NULL) == -1) {
+			perror("Select:");
+			exit(1);
+		}
+		readLoop(master, readFds, listener, fdMax);
+	}
+	FD_ZERO(&master);
+	FD_ZERO(&readFds);
 	close(listener);
 	return (1);
 }
