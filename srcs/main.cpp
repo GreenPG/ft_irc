30f79b6ef7dab1ff3515ea31aea8dd115476e7e7
Gillian Pasquette <gpasquet@1E6.42angouleme.fr>
2023-10-11 17:15:58 +0200

    correct parsing

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tlarraze <tlarraze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 17:16:22 by gpasquet          #+#    #+#             */
-/*   Updated: 2023/10/10 17:54:38 by gpasquet         ###   ########.fr       */
+/*   Updated: 2023/10/11 16:27:43 by gpasquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ struct addrinfo initHints(void) {
 	hints.ai_family	= AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
+	hints.ai_protocol = IPPROTO_TCP;
 	return (hints);	
 }
 
@@ -115,12 +116,10 @@ void	receiveData(fd_set &master, int &listener, int &fdMax, int &socketFd, std::
 
 	}
 	else {
-		std::cout << "before recv: " << buf << std::endl;
 		currentUser = identifyUser(socketFd, user_list);
 		if ((nbytes = recv(socketFd, buf, sizeof(buf), 0)) <= 0)
 			receiveError(nbytes, socketFd, master);
 		else {
-			std::cout << "after recv: " << buf << std::endl;
 			parser(buf, currentUser);
 			std::cout << buf << std::endl;
 			for (int j = 0; j <= fdMax; j++) {
--- srcs/parser.cpp
+++ srcs/parser.cpp
@@ -6,30 +6,49 @@
 /*   By: gpasquet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 13:33:01 by gpasquet          #+#    #+#             */
-/*   Updated: 2023/10/10 17:53:01 by gpasquet         ###   ########.fr       */
+/*   Updated: 2023/10/11 16:26:28 by gpasquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/irc.hpp"
 
+static std::vector<std::string>	splitInput(std::string input) {
+	size_t						end;
+	size_t						start  = 0;
+	std::vector<std::string>	cmdList;
+
+	while (start < input.npos) {  
+		end = input.find_first_of("\r\n", start);
+		if (end != input.npos)
+			cmdList.push_back(input.substr(start, end - start));
+		start = input.find_first_not_of("\r\n", end);
+	}
+	return (cmdList);
+}
+
 void	parser(const std::string input, user &currentUser) {
-	std::string cmd;
-	std::string	args;
-	unsigned	i; 
+	std::vector<std::string>	cmdList;
+	std::string 				cmd;
+	std::string					args;
+	unsigned int				j; 
 
 	int							spaceIdx;
-	const std::string			cmdArr[] = {"PASS", "NICK", "USER", "PRIVMSG", "JOIN", "MODE", "INVITE", "TOPIC", "KICK"};
-		std::vector<std::string> 	cmdsVec(cmdArr, cmdArr + sizeof(cmdArr) / sizeof(cmdArr[0]));
+	const std::string			cmdArr[] = {"CAP", "PASS", "NICK", "USER", "PRIVMSG", "JOIN", "MODE", "INVITE", "TOPIC", "KICK"};
+	std::vector<std::string> 	cmdsVec(cmdArr, cmdArr + sizeof(cmdArr) / sizeof(cmdArr[0]));
 
-	spaceIdx = input.find(' ');
-	cmd = input.substr(0, spaceIdx);
-	args = input.substr(spaceIdx + 1, input.size() - spaceIdx);
-	for (i = 0; i < cmdsVec.size(); i++) { 
-		if (cmdsVec[i] == cmd) {
-			std::cout << "command " << cmdsVec[i] << " send by " << currentUser.get_nickname() << std::endl; 
-			break;
+	cmdList = splitInput(input);
+	for (unsigned int i = 0; i < cmdList.size(); i++) {
+		spaceIdx = cmdList[i].find(' ');
+		cmd = cmdList[i].substr(0, spaceIdx);
+		args = cmdList[i].substr(spaceIdx + 1, input.size() - spaceIdx);
+		for (j = 0; j < cmdsVec.size(); j++) { 
+			if (cmdsVec[j] == cmd) {
+				std::cout << "command " << cmdsVec[j] << " send by " << currentUser.get_nickname() << std::endl; 
+				std::cout << "args: " << args << std::endl;
+				break;
+			}
 		}
+		if (j == cmdsVec.size())
+			std::cout << "Unknown command" << std::endl;
 	}
-	if (i == cmdsVec.size())
-		std::cout << "Unknown command" << std::endl;
 }
