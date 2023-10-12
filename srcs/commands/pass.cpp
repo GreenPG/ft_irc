00077598abe54c8cf50869acbd791a6ad0aa1256
Theo Larrazet <tlarraze@1F4.42angouleme.fr>
2023-10-12 15:34:56 +0200

    added replies.hpp message to join

--- srcs/commands/pass.cpp
+++ srcs/commands/pass.cpp
@@ -6,7 +6,7 @@
 /*   By: tlarraze <tlarraze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 14:32:19 by tlarraze          #+#    #+#             */
-/*   Updated: 2023/10/12 15:28:42 by tlarraze         ###   ########.fr       */
+/*   Updated: 2023/10/12 15:33:57 by tlarraze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	pass(std::string args, Server &server, user &user)
 {
-	if (args != server.getPassword())
+	if (user.get_password_check() == 0)
+		sendMessage("ERR_ALREADYREGISTERED\n", user);
+	else if (args != server.getPassword())
 	{
-		sendMessage("ERR_PASSWDMISMATCH (464)\n", user);
+		sendMessage(ERR_PASSWDMISMATCH(user.get_nickname()).c_str(), user);
 		return ;
 	}
-	else if (user.get_password_check() == 0)
-		sendMessage("ERR_ALREADYREGISTERED\n", user);
 	else if (user.get_password_check() != 0)
 		user.set_password_check();
 }
