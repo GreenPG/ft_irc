0fefffb30fe24b1ee3bac7503c2f64d4f19f72dd
Theo Larrazet <tlarraze@1B1.42angouleme.fr>
2023-10-13 14:11:52 +0200

    added \n to errors msg

--- includes/replies.hpp
+++ includes/replies.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   replies.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gpasquet <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: tlarraze <tlarraze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 13:43:58 by gpasquet          #+#    #+#             */
-/*   Updated: 2023/10/13 10:29:14 by gpasquet         ###   ########.fr       */
+/*   Updated: 2023/10/13 14:10:50 by tlarraze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,34 +17,34 @@
 #define	RPL_PRIVMSG(sender, target, msg) (":" + sender + " PRIVMSG " + target + " :" + msg + "\r\n")
 
 //////////////////////////////RPL MESSAGES///////////////////////////////////////////////////
-# define RPL_WELCOME(nick, user) ("001 " + nick " :Welcome to the ircserv Network, " + nick + "!" + user)
-# define RPL_NOTOPIC(nick, channel) ("331 " + nick + " " + channel + " :No topic is set")
-# define RPL_TOPIC(nick, channel, topic) ("332 " + nick + " " + channel + " :" + topic)
-# define RPL_INVITING(nick, invited, channel) ("341 " + nick + " " + invited + " " + channel)
-# define RPL_NAMREPLY(nick, channel, prefix, member) ("352 " + nick + " " + "#" + channel + " : " + prefix + member)
-# define RPL_ENDOFNAMES(nick, channel) ("366 " + nick + " " + channel + " :End of /NAMES list")
+# define RPL_WELCOME(nick, user) ("001 " + nick " :Welcome to the ircserv Network, " + nick + "!" + user + "\r\n")
+# define RPL_NOTOPIC(nick, channel) ("331 " + nick + " " + channel + " :No topic is set" + "\r\n")
+# define RPL_TOPIC(nick, channel, topic) ("332 " + nick + " " + channel + " :" + topic + "\r\n")
+# define RPL_INVITING(nick, invited, channel) ("341 " + nick + " " + invited + " " + channel + "\r\n")
+# define RPL_NAMREPLY(nick, channel, prefix, member) ("352 " + nick + " " + "#" + channel + " : " + prefix + member + "\r\n")
+# define RPL_ENDOFNAMES(nick, channel) ("366 " + nick + " " + channel + " :End of /NAMES list" + "\r\n")
 
 
 
 ///////////////////////////////ERRORS MESSAGES/////////////////////////////////////////////
-# define ERR_NOSUCHNICK(nick, name) ("401 " + nick + " " + name + " :No such nick/channel")
-# define ERR_NOSUCHCHANNEL(nick, channel) ("403 " + nick + " " + channel + " :No such channel")
-# define ERR_CANNOTSENDTOCHAN(nick, channel) ("404 " + nick + " " + channe  " :Cannot send to channel")
-# define ERR_BADCHANNELKEY(nick, channel) ("405 " + nick + " " + channel + " :Cannot join channel (+k)")
-# define ERR_NORECIPIENT(nick, cmd) ("411 " + nick + " :No recipient given (" + cmd + ")" )
-# define ERR_NOTEXTTOSEND(nick) ("412 " + nick + " :No text to send")
-# define ERR_NONICKNAMEGIVEN(nick) ("431 " + nick " :No nickname given")
-# define ERR_ERRONEUSNICKNAME(oldNick, newNick) ("432 " + oldNick + " " + newNick " :Erroneus nickname")
-# define ERR_NICKNAMEINUSE(oldNick, newNick) ("433 " + oldNick + " " newNick + " :Nickname is already in use")
-# define ERR_USERNOTINCHANNEL(nick, target, channel) ("441 " + nick + " " + target + " " + channel + " :They aren't on that channel" )
-# define ERR_NOTONCHANNEL(nick, channel) ("442 " + nick + " " + channel + " :You're not ont that channel")
-# define ERR_USERONCHANNEL(nick, invited, channel) ("443 " + nick + " " + invited + " " + channel + " :is already on channel")
-# define ERR_CHANOPRIVSNEEDED(nick, channel) ("482 " + nick + " " + channel + " :You're not channel operator")
-# define ERR_NEEDMOREPARAMS(nick, cmd) ("461 " + nick + cmd + " : Not enough parameters")
-# define ERR_ALREADYREGISTERED(nick) ("462 " + nick + " : You may not be reregister")
-# define ERR_PASSWDMISMATCH(nick) ("464 " + nick + "Password incorrect")
-# define ERR_CHANNELISFULL(nick, channel) ("471 " + nick + " " + channel + " :Cannot join channel (+l)")
-# define ERR_INVITEONLYCHAN(nick, channel) ("473 " + nick + " " + channel " :Cannot join channel (+i)")
+# define ERR_NOSUCHNICK(nick, name) ("401 " + nick + " " + name + " :No such nick/channel" + "\r\n")
+# define ERR_NOSUCHCHANNEL(nick, channel) ("403 " + nick + " " + channel + " :No such channel" + "\r\n")
+# define ERR_CANNOTSENDTOCHAN(nick, channel) ("404 " + nick + " " + channe  " :Cannot send to channel" + "\r\n")
+# define ERR_BADCHANNELKEY(nick, channel) ("405 " + nick + " " + channel + " :Cannot join channel (+k)" + "\r\n")
+# define ERR_NORECIPIENT(nick, cmd) ("411 " + nick + " :No recipient given (" + cmd + ")"  + "\r\n")
+# define ERR_NOTEXTTOSEND(nick) ("412 " + nick + " :No text to send" + "\r\n")
+# define ERR_NONICKNAMEGIVEN(nick) ("431 " + nick " :No nickname given" + "\r\n")
+# define ERR_ERRONEUSNICKNAME(oldNick, newNick) ("432 " + oldNick + " " + newNick " :Erroneus nickname" + "\r\n")
+# define ERR_NICKNAMEINUSE(oldNick, newNick) ("433 " + oldNick + " " newNick + " :Nickname is already in use" + "\r\n")
+# define ERR_USERNOTINCHANNEL(nick, target, channel) ("441 " + nick + " " + target + " " + channel + " :They aren't on that channel"  + "\r\n")
+# define ERR_NOTONCHANNEL(nick, channel) ("442 " + nick + " " + channel + " :You're not ont that channel" + "\r\n")
+# define ERR_USERONCHANNEL(nick, invited, channel) ("443 " + nick + " " + invited + " " + channel + " :is already + "\r\n" on channel")
+# define ERR_CHANOPRIVSNEEDED(nick, channel) ("482 " + nick + " " + channel + " :You're not channel operator" + "\r\n")
+# define ERR_NEEDMOREPARAMS(nick, cmd) ("461 " + nick + cmd + " : Not enough parameters" + "\r\n")
+# define ERR_ALREADYREGISTERED(nick) ("462 " + nick + " : You may not be reregister" + "\r\n")
+# define ERR_PASSWDMISMATCH(nick) ("464 " + nick + "Password incorrect" + "\r\n")
+# define ERR_CHANNELISFULL(nick, channel) ("471 " + nick + " " + channel + " :Cannot join channel (+l)" + "\r\n")
+# define ERR_INVITEONLYCHAN(nick, channel) ("473 " + nick + " " + channel " :Cannot join channel (+i)" + "\r\n")
 
 
 #endif
