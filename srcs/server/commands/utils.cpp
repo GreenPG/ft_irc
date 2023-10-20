8ed26a69b33ab2f681248204edd1afdc0795c116
Theo Larrazet <tlarraze@1E2.42angouleme.fr>
2023-10-20 13:34:17 +0200

    changed errors msg for invalid args

--- srcs/server/commands/utils.cpp
+++ srcs/server/commands/utils.cpp
@@ -6,39 +6,35 @@
 /*   By: tlarraze <tlarraze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 13:34:54 by tlarraze          #+#    #+#             */
-/*   Updated: 2023/10/19 14:25:21 by gpasquet         ###   ########.fr       */
+/*   Updated: 2023/10/20 13:33:47 by tlarraze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/irc.hpp"
 
-int is_num(std::string s)
+int	is_num(std::string s)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (s[i])
-    {
-        if (s[i] < '0' || s[i] > '9')
-            return (1);
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-int check_args(int argc, char **argv)
+int	check_args(int argc, char **argv)
 {
-	if (argc == 2) {
-		std::cout << "Port number expected" << std::endl;
+	if (argc != 3) {
+		std::cout << "Wrong number of args  \"./ircserv <port> <password>\"" << std::endl;
 		return (1);
 	}
-    if (argc != 3) {
-		std::cout << "Wrong number of args  \"./ircserv <port> <password>\"" << std::endl;
-        return (1);
-    }
-    if (is_num(argv[1]) == 1) {
+	if (is_num(argv[1]) == 1) {
 		std::cout << "Port can only be numbers" << std::endl;
-        return (1);
-    }
-    return (0);
+		return (1);
+	}
+	return (0);
 }
