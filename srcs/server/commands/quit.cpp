035f40b28ae6caee09779ed97cccca6d9b46ffb3
Theo Larrazet <tlarraze@1D2.42angouleme.fr>
2023-10-23 13:59:53 +0200

    fix leak if quit used

--- srcs/server/commands/quit.cpp
+++ srcs/server/commands/quit.cpp
@@ -6,7 +6,7 @@
 /*   By: tlarraze <tlarraze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 15:15:31 by tlarraze          #+#    #+#             */
-/*   Updated: 2023/10/23 11:03:04 by gpasquet         ###   ########.fr       */
+/*   Updated: 2023/10/23 13:59:02 by tlarraze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 void	quit(std::string args, Server &server, User &currentUser) {
 
+	User	*u;
+
+	u = &currentUser;
 	if (args != "")
 		server.sendMessageToServer(RPL_QUIT(currentUser.get_nickname(), args));
 	else
@@ -21,5 +24,6 @@ void	quit(std::string args, Server &server, User &currentUser) {
 	server.remove_every_trace_of_user(&currentUser);
 	FD_CLR(currentUser.get_fd_socket(), &server.getMaster());
 	close(currentUser.get_fd_socket());
+	delete u;
 	server.getUserList()->erase(server.getUserList()->begin() + get_user_pos(*server.getUserList(), &currentUser));
 }
